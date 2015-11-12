62a89327072a057e0f3b1e77b5f95fe6efc07f27
Teck Loy Low <teckloy.low@thetrainline.com>
2015-11-12 14:03:31 +0000

    initial spike

---
+++ NLog.Targets.SNS/AwsCredentialResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Amazon.Runtime;
+
+namespace NLog.Targets.SNS
+{
+    public interface IAwsCredentialResolver
+    {
+        AWSCredentials ResolveFor(string credentialTypeString);
+    }
+
+    public class AwsCredentialResolver: IAwsCredentialResolver
+    {
+        public AWSCredentials ResolveFor(string credentialTypeString)
+        {
+            Type credentialType = null;
+
+            if (!string.IsNullOrWhiteSpace(credentialTypeString))
+                credentialType = Type.GetType(credentialTypeString);
+
+            if (credentialType == null)
+                credentialType = typeof (BasicAWSCredentials);
+
+            return (AWSCredentials) Activator.CreateInstance(credentialType);
+        }
+    }
+}
